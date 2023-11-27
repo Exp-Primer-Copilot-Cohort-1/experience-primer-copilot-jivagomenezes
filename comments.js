a469da815b6356f92c774cee2db52e66c1b867ba
jivagomenezes <97550741+jivagomenezes@users.noreply.github.com>
2023-11-27 23:59:16 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,11 +1,40 @@
-const http = require("http");
+// Create web server
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader("Content-Type", "text/plain");
-  res.end("Hello, world!");
+const express = require("express");
+const bodyParser = require("body-parser");
+const { randomBytes } = require("crypto");
+const cors = require("cors");
+
+// Create a new express application
+const app = express();
+// Parse incoming request bodies in a middleware before your handlers, available under the req.body property.
+app.use(bodyParser.json());
+// Enable CORS
+app.use(cors());
+
+const commentsByPostId = {};
+
+app.get("/posts/:id/comments", (req, res) => {
+  res.send(commentsByPostId[req.params.id] || []);
 });
 
-server.listen(3000, "localhost", () => {
-  console.log("Server running at http://localhost:3000/");
+// Create a new comment
+app.post("/posts/:id/comments", (req, res) => {
+  const commentId = randomBytes(4).toString("hex");
+  // req.body is a JSON object that will be sent by the client
+  const { content } = req.body;
+  // Get the comments array for the post
+  const comments = commentsByPostId[req.params.id] || [];
+  // Add the new comment
+  comments.push({ id: commentId, content });
+  // Update the comments array
+  commentsByPostId[req.params.id] = comments;
+
+  // Send back the new comment
+  res.status(201).send(comments);
+});
+
+// Listen on port 4001
+app.listen(4001, () => {
+  console.log("Listening on 4001");
 });
